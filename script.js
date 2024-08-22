1506271ffa1a861faedcd6f2e99990959e96444c
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-22 15:24:11 +0200

    prompt

---
+++ script.js
@@ -0,0 +1,3 @@
+// chiedo all'utente di inserire una parola, faccio un prompt
+let word = prompt("Inserisci una parola:");
+// cerco di capire se la parola è palindroma con una funzione e faccio un for
