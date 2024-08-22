f259b79574972156c8e6823948c7be526ef1b883
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-22 16:46:12 +0200

    scelta dell'utente

--- es2/es2script.js
+++ es2/es2script.js
@@ -1,1 +1,5 @@
 console.log('loggo')
+
+// chiedo all'utente pari e dispari e il numero
+const userChoice = prompt('Choose "even" or "odd":').toLowerCase();
+let userNumber = parseInt(prompt('Enter a number between 1 and 5:'));
---
+++ es2script.js
@@ -0,0 +1,3 @@
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+const userChoice = prompt('Pari o dispari?:');
+let userNumber = parseInt(prompt('Scegli un numero tra 1 e 5:'));
