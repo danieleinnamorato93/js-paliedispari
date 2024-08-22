78efc58b65d3d4d91108d4d4089770dea0125373
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-22 15:56:29 +0200

    esercizio 1 terminato

--- es1script.js
+++ es1script.js
@@ -1,5 +1,5 @@
 // chiedo all'utente di inserire una parola, faccio un prompt
-let word = prompt("Inserisci una parola:");
+let word = prompt("Inserisci una parola palindroma:");
 
 //faccio una funzione per verificare se è palindroma: prendo la parola, ne separo le lettere, ne inverto l'ordine e li riunisco in stringa
 
@@ -8,3 +8,9 @@ function isPalindrome(word) {
     return word === reversedWord;  
 }
 
+// uso la funzione e mostro il risultato
+if(isPalindrome(word)) {
+    console.log('Daje, è una parola palindroma!')
+} else {
+    console.log('Mi dispiace ma non è una parola palindroma!')
+}
