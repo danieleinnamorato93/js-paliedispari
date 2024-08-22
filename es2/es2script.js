8a616ee8d5e7f4bd6f3d0d6579ce1bf427fc867e
danieleinnamorato93 <daniele.innamorato93@gmail.com>
2024-08-22 17:30:29 +0200

    esercizio terminato

--- es2/es2script.js
+++
@@ -1,5 +0,0 @@
-console.log('loggo')
-
-// chiedo all'utente pari e dispari e il numero
-const userChoice = prompt('Choose "even" or "odd":').toLowerCase();
-let userNumber = parseInt(prompt('Enter a number between 1 and 5:'));
